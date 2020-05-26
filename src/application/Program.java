28089ee2667ab7d3272c11ee7ce20f7f1c2f59da
Sergio Pelegrini <serpel@gmail.com>
2020-05-26 15:22:11 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Entre com 2 números inteiros: ");
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		int[][] mat = new int[m][n];
+		
+		for (int i=0;i<mat.length;i++) {
+			for (int j=0;j<mat[i].length;j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		
+		int k = sc.nextInt();
+		
+        for (int i=0;i<mat.length;i++) {
+        	for (int j=0;j<mat[i].length;j++) {
+        		if (mat[i][j] == k) {
+        			System.out.println("Posição: " + i + "," + j);
+        			if (j > 0) {
+        				System.out.println("Esquerda: " + mat[i][j-1]);
+        			}
+        			if (i > 0) {
+        				System.out.println("Acima: " + mat[i-1][j]);
+        			}
+        			if (j < mat[i].length-1) {
+        				System.out.println("Direita: " + mat[i][j+1]);
+        			}
+        			if (i < mat.length-1) {
+        				System.out.println("Abaixo: " + mat[i+1][j]);
+        			}
+        		}
+        	}
+        }
+		
+		sc.close();
+	}
+
+}
